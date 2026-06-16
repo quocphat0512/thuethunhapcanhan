6e48642534455767c0e6ed970b281d91e73e3d59
quocphat0512 <quocphat0512@gmail.com>
2026-06-16 19:07:19 +0700

    Update thue.py

--- thue.py
+++ thue.py
@@ -8,9 +8,9 @@ tong_thu_nhap= st.number_input("Nhập thu nhập của bạn ")
 so_nguoi_phu_thuoc= st.number_input("số người phụ thuộc")
 if tong_thu_nhap > 0:
     
-giam_tru_ban_than=15500000
-giam_tru_phu_thuoc=6200000
-tobg_giam_tru=giam_tru_ca_nhan + giam_tru_phu_thuoc
+    giam_tru_ban_than=15500000
+    giam_tru_phu_thuoc=6200000
+    tong_giam_tru=giam_tru_ca_nhan + giam_tru_phu_thuoc
 a = tong_thu_nhap + tong_giam_tru
 c=0
 if a > 0:
