06265a171b41a57d8cba3319fd2672b13f1ea9db
quocphat0512 <quocphat0512@gmail.com>
2026-06-16 19:40:01 +0700

    Update thue.py

--- thue.py
+++ thue.py
@@ -9,7 +9,7 @@ so_nguoi_phu_thuoc = st.number_input("Số người phụ thuộc", min_value=0,
 bao_hiem_xa_hoi_bat_buoc = 0.15*tong_thu_nhap
 giam_tru_ban_than = 11000000.0
 giam_tru_phu_thuoc = 4400000.0 * so_nguoi_phu_thuoc
-tong_giam_tru = giam_tru_ban_than + giam_tru_phu_thuoc + bao_hiem_â_hoi_bat_buoc
+tong_giam_tru = giam_tru_ban_than + giam_tru_phu_thuoc + bao_hiem_xa_hoi_bat_buoc
 a = 0.0
 c = 0.0
 
