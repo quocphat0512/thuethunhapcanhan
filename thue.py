62cbb73cf3937ad043a248bfab936106deb26f8a
quocphat0512 <quocphat0512@gmail.com>
2026-06-16 19:03:47 +0700

    Update thue.py

--- thue.py
+++ thue.py
@@ -4,8 +4,14 @@ st.title("🧮 Tính thuế thu nhập cá nhân")
 
 st.divider()
 
-a = st.number_input("Nhập thu nhập tính thuế của bạn ")
-
+tong_thu_nhap= st.number_input("Nhập thu nhập của bạn ")
+so_nguoi_phu_thuoc= st.number_input("số người phụ thuộc")
+if tong_thu_nhap>0
+giam_tru_ban_than=15500000
+giam_tru_phu_thuoc=6200000
+tobg_giam_tru=giam_tru_ca_nhan + giam_tru_phu_thuoc
+a = tong_thu_nhap + tong_giam_tru
+c=0
 if a > 0:
     c = 0.0  
 
